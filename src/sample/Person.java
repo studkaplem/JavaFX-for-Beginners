c27b7437b8ce3e2af8a74fd776cd8d9bae2c1a18
Emre Kaplan <emre.kaplan@hs-augsburg.de>
2021-05-29 16:04:40 +0200

    first javafx application

---
+++ src/sample/Person.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Person {
+
+    private String firstName, lastName;
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
